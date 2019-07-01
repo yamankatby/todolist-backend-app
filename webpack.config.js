a74cb2505f6cf47ca26e174957ac6c72c026eb61
Yaman KATBY <m.yaman.katby@gmail.com>
2019-07-01 15:08:22 +0300

    Work on Deploy to Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { NODE_ENV = 'production' } = process.env;
+
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -23,5 +24,5 @@ module.exports = {
 	resolve: {
 		extensions: ['.ts', '.js'],
 	},
-	externals: [ nodeExternals() ]
+	externals: [nodeExternals()],
 };
