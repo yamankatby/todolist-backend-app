b41a896c6a28def9dc253e06ccd13987c981467e
Yaman KATBY <m.yaman.katby@gmail.com>
2019-10-01 14:56:04 +0300

    🎉 Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import 'babel-polyfill';
+const sayHello = (name: string) => {
+	return `Hello, ${name}!`;
+};
 
-import mongoose from 'mongoose';
-
-import { database_url, port } from './config/constants';
-import app from './app';
-
-mongoose.connect(database_url, { useNewUrlParser: true }).then(() => {
-	console.warn('Mongoose connected to the Database');
-});
-mongoose.connection.on('error', (e) => {
-	console.error(`Error during connecting to Mongoose _ ${e.message}`);
-});
-
-app.set('port', port);
-app.listen(app.get('port'), () => {
-	console.warn(`Express running on PORT ${port}`);
-});
+sayHello('Yaman KATBY');
