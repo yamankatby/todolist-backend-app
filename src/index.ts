6d4ed110a30301ed8665ab68144f93a89cfa87ff
Yaman KATBY <m.yaman.katby@gmail.com>
2019-10-01 15:20:09 +0300

    🏗 Add work files structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-const sayHello = (name: string) => {
-	return `Hello, ${name}!`;
-};
+import express from 'express';
 
-sayHello('Yaman KATBY');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+	response.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
+});
